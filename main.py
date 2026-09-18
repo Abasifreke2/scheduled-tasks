3b46de04953230c9910a0f669f086c9d2d6f8c24
Abasifreke Editi <abasifrekeediti14@gmail.com>
2026-09-18 20:29:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,34 +5,30 @@
 # 4. Update birthdays.csv to contain today's month and day.
 # See the solution video in the 100 Days of Python Course for explainations.
 
-
+import os
+import random
 from datetime import datetime
 import pandas
-import random
 import smtplib
-import os
 
-# import os and use it to get the Github repository secrets
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
 today = datetime.now()
-today_tuple = (today.month, today.day)
+today_tuple = (today.month,today.day)
 
 data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
+birthdays_dict = {(data_row["month"],data_row["day"]): data_row for (index,data_row) in data.iterrows()}
+
 if today_tuple in birthdays_dict:
     birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+    file_path = f"./letter_templates/letter_{random.randint(1,3)}.txt"
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+    with open(file_path) as letter:
+        content = letter.read()
+        content = content.replace("[NAME]",birthday_person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com",587) as connection:
         connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+        connection.login(user=MY_EMAIL,password=MY_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL,to_addrs=birthday_person["email"],msg=f"Subject:Happy Birthday\n\n {content}")
